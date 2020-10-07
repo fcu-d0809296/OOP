eb747e0ccf74b2817c6008a3d874399fea58cc1e
fcu-d0809296 <jiannweyfcutw@mail.fcu.edu.tw>
2020-10-07 21:29:01 +0800

    CheckOddEven Done

--- src/CheckOddEven.java
+++ src/CheckOddEven.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class CheckOddEven {
+  /**.*/
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    System.out.println("Enter a integer: ");
+    int a = scanner.nextInt();
+    if (a % 2 == 0) {
+      System.out.println("The input integer is Even Number.");
+    } else {
+      System.out.println("The input integer is Odd Number.");
+    }
+    scanner.close();
+  }
 
 }
