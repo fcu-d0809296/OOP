7518a24757cb1eab6bcaf70435356956396c83f1
fcu-d0809296 <jiannweyfcutw@mail.fcu.edu.tw>
2020-10-07 21:58:01 +0800

    StringComparator Done

---
+++ src/StringComparator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class StringComparator {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a string1: ");
+    String s1 = scanner.next();
+    System.out.println("Enter a string2: ");
+    String s2 = scanner.next();
+    
+    if (s1.compareTo(s2) > 0) {
+      System.out.println("The two strings are the same.");
+    } else {
+      System.out.println("The two strings are not the same.");
+    }
+  }
+
+}
