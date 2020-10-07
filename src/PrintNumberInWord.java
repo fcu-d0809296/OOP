5af65deaef521c28c288e8eff6e0743f054d1c93
fcu-d0809296 <jiannweyfcutw@mail.fcu.edu.tw>
2020-10-07 22:13:25 +0800

    Scanner close

--- src/PrintNumberInWord.java
+++ src/PrintNumberInWord.java
@@ -14,6 +14,7 @@ public class PrintNumberInWord {
     }else {
       System.out.printf("\"%s\"",list[i-1]);
     }
+    scanner.close();
   }
 
 }
--- src/StringComparator.java
+++ src/StringComparator.java
@@ -14,6 +14,7 @@ public class StringComparator {
     } else {
       System.out.println("The two strings are not the same.");
     }
+    scanner.close();
   }
-
+  
 }
