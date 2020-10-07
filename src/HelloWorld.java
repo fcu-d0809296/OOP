391873daf89b416546c7e33eab8da31e9be466a3
fcu-d0809296 <jiannweyfcutw@mail.fcu.edu.tw>
2020-10-07 21:28:38 +0800

    include check style

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,9 @@
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	}
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
 
 }
--- src/KeyboardScanner.java
+++ src/KeyboardScanner.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
 public class KeyboardScanner {
-
-  public static void main(String[] args) {
-  // TODO Auto-generated method stub
+  /** . */
+  public static void main(String[] args) { 
+    //  Gets the distance between tw points.
     Scanner scanner = new Scanner(System.in);
 
-  	System.out.println("Enter a integer: ");
-  	int a = scanner.nextInt();
+    System.out.println("Enter a integer: ");
+    int a = scanner.nextInt();
     System.out.println("Enter a float point number: ");
     float b = scanner.nextFloat();
     System.out.println("Enter a you name: ");
-  	String c = scanner.next();
-  	System.out.printf("Hi %s, the multiplication of %d and %f is %.2e", c,a,b,b);
-  	scanner.close(); 
+    String c = scanner.next();
+    System.out.printf("Hi %s, the multiplication of %d and %f is %.2e", c, a, b, b);
+    scanner.close();
   }
-  	
+
 }
+
